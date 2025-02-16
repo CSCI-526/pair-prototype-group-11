99aec662dcc3076fde0661d7deb5e7a19127f395
seeratparmar <seeratparmar@gmail.com>
2025-02-16 15:51:11 -0800

    updating destroy and new elements coming in
    potential bug: check if the falling is changing box index

--- matchbox/Assets/Scripts/Match3/Board.cs
+++ matchbox/Assets/Scripts/Match3/Board.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using System.Threading.Tasks;
+using UnityEditor.Tilemaps;
 
 public sealed class Board : MonoBehaviour
 {
@@ -34,7 +35,7 @@ public sealed class Board : MonoBehaviour
                 box.x_coord = i;
                 box.y_coord = j;
 
-                Debug.Log("Box: " + i + " " + j);
+                //Debug.Log("Box: " + i + " " + j);
 
                 do
                 { int index = Random.Range(0, IconDb.Icons.Length);
@@ -125,6 +126,44 @@ public sealed class Board : MonoBehaviour
 
     public async Task SwapBoxes(Box box1, Box box2)
 {
+
+    await BoxSwapMechanic(box1, box2); // initial swap
+    MatchingAlgo.Index Index;
+    Index.X = box1.x_coord;
+    Index.Y = box1.y_coord;
+    List<MatchingAlgo.Index> Indexes = new List<MatchingAlgo.Index>();
+    List<IMatchInterface> Boxes = new List<IMatchInterface>();
+
+    bool box1_Match= MatchingAlgo.CheckMatch(Index, Board.Instance.Boxes, ref Indexes, ref Boxes);
+    Debug.Log(box1_Match);
+    
+
+    MatchingAlgo.Index Index2;
+    Index2.X = box2.x_coord;
+    Index2.Y = box2.y_coord;
+    List<MatchingAlgo.Index> Indexes2 = new List<MatchingAlgo.Index>();
+    List<IMatchInterface> Boxes2 = new List<IMatchInterface>();
+    bool box2_Match= MatchingAlgo.CheckMatch(Index2, Board.Instance.Boxes, ref Indexes2, ref Boxes2);
+    
+    Debug.Log( box2_Match);
+    if (!box1_Match && !box2_Match)
+    {
+        await Task.Delay(500);
+        await BoxSwapMechanic(box1, box2);// swaps back if no match
+    }
+
+    if(Indexes.Count>0 || Indexes2.Count>0)
+    {
+        PopulateDestroyedBoxes(Indexes, Indexes2);
+    }
+
+
+
+    
+}
+
+    public async Task BoxSwapMechanic(Box box1, Box box2)
+    {
     var arrow1 = box1.arrow;
     var arrow2 = box2.arrow;
 
@@ -146,8 +185,77 @@ public sealed class Board : MonoBehaviour
     var tempIcon = box1.Icon;
     box1.Icon = box2.Icon;
     box2.Icon = tempIcon;
+    await Task.Delay(1000);
+    Debug.Log("Swapped"+ box1.x_coord + " " + box1.y_coord + "and " + box2.x_coord + " " + box2.y_coord);
+    }
 
+    public List<MatchingAlgo.Index> allIndexes;
+public async void PopulateDestroyedBoxes(List<MatchingAlgo.Index> Indexes1, List<MatchingAlgo.Index> Indexes2)
+{
+    
 
+    allIndexes= Indexes1.Concat(Indexes2).Distinct().ToList();
+    allIndexes = allIndexes.OrderByDescending(index => index.X).ToList();
+
+    foreach (var index in allIndexes)
+    {
+        int x = index.X;
+        int y = index.Y;
+
+       
+        Boxes[x, y] = null;
+    }
+
+    for (int y = 0; y < no_of_cols; y++)
+    {
+        bool checkFlag= true;
+        
+        while(checkFlag){
+            checkFlag= false;
+            for (int x = no_of_rows - 1; x >= 0; x--)
+            {
+                if (Boxes[x, y] == null)
+                {
+                    // Shift boxes  down
+                    for (int k = x; k > 0; k--)
+                    {
+                        if (Boxes[k - 1, y] != null)
+                        {
+                            
+                            await BoxSwapMechanic(rows[k].boxes[y], rows[k - 1].boxes[y]);
+                            Boxes[k, y] = Boxes[k - 1, y];
+                            Boxes[k - 1, y] = null;
+                            checkFlag= true;
+                        }
+                    }
+                    // Generate a new box at the top
+                    Boxes[0, y] = GenerateNewBox(0, y);
+                  
+                    
+                    
+                }
+            }
+        }
+    }
+}
+
+private Box GenerateNewBox(int i, int j)
+{
+
+    var newBox = rows[i].boxes[j];
+
+    newBox.x_coord = i;
+    newBox.y_coord = j;
+
+    Debug.Log("Box: " + i + " " + j);
+
+    int index = Random.Range(0, IconDb.Icons.Length);
+    
+    newBox.Icon = IconDb.Icons[index];
+    newBox.Color = index.ToString();
+
+    return newBox;
+}
 
 
     await Task.Delay(10); 
