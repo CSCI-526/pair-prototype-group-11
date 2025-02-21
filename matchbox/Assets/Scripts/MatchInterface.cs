0613cd2917b3329db408606ee9c325f909f8b079
seeratparmar <seeratparmar@gmail.com>
2025-02-20 18:27:40 -0800

    deploy

--- matchbox/Assets/Scripts/MatchInterface.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEngine;
-
-public interface IMatchInterface
-{ 
-    public string Color { get; set; }
-}
